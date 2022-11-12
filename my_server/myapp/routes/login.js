cd708130bcb8d5b40130559cab8211c68776cba4
kevingilman01 <kevingilman01@gmail.com>
2022-11-11 20:15:23 -0700

    posting user data

--- my_server/myapp/routes/login.js
+++ my_server/myapp/routes/login.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
+var Physician = require('../models/Physician');
+var errorMsg1 = 'Login Failed! Please specify all fields.';
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// JUST FOR REFERENCE
+// req.body {
+//     type: Physician or User
+//     username: any valid string
+//     password: any valid string
+// }
+
+router.post('/', function(req, res) {
+    if (!(req.body.type) || !(req.body.username) || !(req.body.password)) {
+        res.status(400).send(errorMsg1);
+    }
+    else if (req.body.type === 'user') {
+
+    }
+    else if (req.body.type === 'physician') {
+
+    }
+    else {
+        res.status(400).send('Neither User nor Physician');
+    }
+})
 
 module.exports = router;
